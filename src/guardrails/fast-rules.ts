d93e344bf7b553de902d508f4c2b4205c5f0eea8
Richard <ricardoisv84@gmail.com>
2025-10-24 11:15:02 -0700

    fix: remove unused fuzzy-matcher import
    Remove import for fuzzy-matcher.ts which was deleted in cleanup.
    This file was not needed and was causing import errors.

--- src/guardrails/fast-rules.ts
+++ src/guardrails/fast-rules.ts
@@ -6,7 +6,6 @@ import type { PolicyRule, PolicyViolation, Logger } from '../types/index.js';
 import { getLogger } from '../config/index.js';
 import { PolicyLoader, PolicyCache } from './policy-loader.js';
 import { CompiledPolicy, PolicyEvaluationResult } from '../types/policies.js';
-import { FuzzyMatcher } from './fuzzy-matcher.js';
 
 export interface FastRulePattern {
   id: string;
